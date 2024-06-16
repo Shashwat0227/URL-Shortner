7d00c4a451707a1ebdd0fa13e19db953d7888273
Shashwat0227 <shashwatchaturvedig@gmail.com>
2024-06-16 15:31:07 +0530

    just a model schema passed

--- server/models/users.js
+++ server/models/users.js
@@ -1,3 +1,24 @@
 const mongoose = require("mongoose");
+const { type } = require("os");
 
-const userSchema = new mongoose.Schema({}, { timestamps: true });
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
